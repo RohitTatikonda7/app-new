65c3bd7148838854fc586c4ca61b38ac28cdce84
RohitTatikonda7 <tatikondarohit7@gmail.com>
2023-05-21 04:09:36 +0530

    commit5
    commit5

--- app1.js
+++ app1.js
@@ -26,22 +26,34 @@ app.post('/', upload.single('file'), async (req, res) => {
     const filePath = req.file.path;
     console.log(req.file.originalname);
 
-    const output = await generateOutput(filePath);
+    let output = await generateOutput(filePath);
     if (!output) {
       console.log('Invalid file');
+      fs.unlink(filePath, (err) => {
+        if (err) {
+          console.error('Error deleting file:', err);
+        } else {
+          console.log('File deleted successfully');
+        }
+      });
       return res.render('index', { errorMessage: 'File not uploaded properly.' });
     }
-
-    const outputBuffer = await fs.readFile('output.xlsx');
-    console.log(outputBuffer);
-    const outputData = xlsx.parse(outputBuffer)[0].data.slice(1);
-    
+    console.log(output)
+    // const outputBuffer = await fs.readFile('output.xlsx');
+    // const outputData = xlsx.parse(outputBuffer)[0].data.slice(1);
+    outputData = xlsx.parse(output)[0].data.slice(1);
     const data = {
       rows: outputData,
       headers: ['Job ID', 'WF Name', 'Description', 'Task ID', 'Parent ID', 'Month', 'Year', 'Topmost Parent Job ID', 'Topmost Parent Workflow Name']
     };
-
     res.render('results', data);
+    fs.unlink(filePath, (err) => {
+      if (err) {
+        console.error('Error deleting file:', err);
+      } else {
+        console.log('File deleted successfully');
+      }
+    });
   } catch (error) {
     console.error(error);
     res.sendStatus(500);
@@ -50,8 +62,16 @@ app.post('/', upload.single('file'), async (req, res) => {
 
 
 app.get('/download', (req, res) => {
-  res.download('output.xlsx');
+  const outputData = req.query.outputData;
+  const outputBuffer = Buffer.from(outputData, 'base64');
+  
+  res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+  res.setHeader('Content-Disposition', 'attachment; filename=output.xlsx');
+  
+  res.send(outputBuffer);
 });
+
+
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
